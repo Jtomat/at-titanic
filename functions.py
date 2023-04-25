805985f3725b1286ba256f4c73c6e6fcffa790ea
Jtomat <stragnik13@gmail.com>
2023-04-25 18:13:38 +0500

    feat: add validator for function find_men_by_age

--- functions.py
+++ functions.py
@@ -5,8 +5,11 @@ class app_functions:
     dataFrame: pd.DataFrame;
     
     def __init__(self):
-        self.dataFrame = pd.read_csv('./assets/titanic.csv');
+        self.dataFrame = pd.read_csv('./assets/data.csv');
         
     def find_men_by_age(self, age: int) -> pd.DataFrame:
+        if age > 60 or age < 30:
+            return Exception('Age out of range [30; 60]');
         data_slice: pd.DataFrame = self.dataFrame.loc[self.dataFrame['Sex'] == 'male'].loc[self.dataFrame['Age'] == age];
         return data_slice[['Name', 'Age', 'Pclass']];
+    
