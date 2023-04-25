a678133203a1e4e0b426155539933dcb4579c2b9
Jtomat <stragnik13@gmail.com>
2023-04-25 18:29:40 +0500

    feat: combine

--- functions.py
+++ functions.py
@@ -7,12 +7,11 @@ class AppFunctions:
     def __init__(self):
         self.dataFrame = pd.read_csv('./assets/titanic.csv')
 
-    def find_men_by_age(self, age: int) -> pd.DataFrame:
-        if age > 60 or age < 30:
+    def find_men_by_age(self, age_min: int, age_max) -> pd.DataFrame:
+        if age_max > 60 or age_min < 30:
             return Exception('Age out of range [30; 60]');
-        data_slice: pd.DataFrame = \
-        self.dataFrame.loc[self.dataFrame['Sex'] == 'male'].loc[
-            self.dataFrame['Age'] == age];
+        data_slice: pd.DataFrame = self.dataFrame.loc[(self.dataFrame['Sex'] == 'male') & (
+            self.dataFrame['Age'] >= age_min) & (self.dataFrame['Age'] <= age_max)];
         return data_slice[['Name', 'Age', 'Pclass']];
 
     def find_passengers_by_name_start(self, name_start: str) -> pd.DataFrame:
