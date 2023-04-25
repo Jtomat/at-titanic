b392c06bc5485074966b4f0f7583bfb370bd7d8c
Igor Krupkin <krupkin.ig@mail.ru>
2023-04-25 18:23:47 +0500

    set actual

--- functions.py
+++ functions.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
 
-class app_functions:
-    dataFrame: pd.DataFrame;
+class AppFunctions:
+    dataFrame: pd.DataFrame
     
     def __init__(self):
-        self.dataFrame = pd.read_csv('./assets/titanic.csv');
+        self.dataFrame = pd.read_csv('./assets/titanic.csv')
         
     def find_men_by_age(self, age: int) -> pd.DataFrame:
-        data_slice: pd.DataFrame = self.dataFrame.loc[self.dataFrame['Sex'] == 'male'].loc[self.dataFrame['Age'] == age];
-        return data_slice[['Name', 'Age', 'Pclass']];
+        data_slice: pd.DataFrame = self.dataFrame.loc[self.dataFrame['Sex'] == 'male'].loc[self.dataFrame['Age'] == age]
+        return data_slice[['Name', 'Age', 'Pclass']]
+
+    def find_passengers_by_name_start(self, name_start: str) -> pd.DataFrame:
+        data_slice: pd.DataFrame = \
+            self.dataFrame.loc[self.dataFrame['Name'].startwith(name_start)]
+        return data_slice[['Name', 'Age', 'Pclass']]
--- spoop.py
+++ spoop.py
@@ -1,38 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import functions
 
+core = functions.AppFunctions()
 DATA_URL = 'assets/data.csv'
 
-@st.cache_data
-def load_data():
-    data = pd.read_csv(DATA_URL)
-    return data
-
-st.title('Uber pickups in NYC')
-
-data_load_state = st.text('Loading data...')
-data = load_data()
-data_load_state.text('Loading data...done!')
+st.title('Приложуха')
 
 st.subheader('Вывести пасажиров с указанным возрастом:')
 
 p_by_age = st.slider('Возраст', 30, 60,0)
-filtered_data = #
+filtered_data = core.find_men_by_age(p_by_age)
 st.map(filtered_data)
 
 
-st.subheader('Имена пасажиров, стоймость билета которых выше:')
-
-ticket_cost = st.number_input('Стоимость билета:')
-filtered_data = #
-st.map(filtered_data)
+# st.subheader('Имена пасажиров, стоймость билета которых выше:')
+#
+# ticket_cost = st.number_input('Стоимость билета:')
+# filtered_data =
+# st.map(filtered_data)
 
 
 st.subheader('Класс, имя и возраст спасенных, имя которых начинается с:')
 
 p_name = st.text_input('Имя:')
-filtered_data = #
+filtered_data = core.find_passengers_by_name_start(p_name)
 st.map(filtered_data)
 
 
