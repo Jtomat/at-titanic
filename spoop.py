ee7b68b6124b9038dae35ec9eb374c8334a7baea
Jtomat <stragnik13@gmail.com>
2023-04-25 18:41:47 +0500

    feat: combine 2

--- spoop.py
+++ spoop.py
@@ -3,30 +3,31 @@ import pandas as pd
 import numpy as np
 import functions
 
-core = functions.AppFunctions()
-DATA_URL = 'assets/data.csv'
 
-st.title('Приложуха')
+DATA_URL = './assets/data.csv';
+core = functions.AppFunctions(DATA_URL);
 
-st.subheader('Вывести пасажиров с указанным возрастом:')
+st.title('Приложуха');
 
-p_by_age = st.slider('Возраст', 30, 60)
-filtered_data = core.find_men_by_age(p_by_age)
-st.map(filtered_data)
+st.subheader('Вывести пасажиров с указанным возрастом:');
 
-
-st.subheader('Имена пасажиров, стоймость билета которых выше:')
-
-ticket_cost = st.number_input('Стоимость билета:')
-filtered_data = core.get_names_by_fare(ticket_cost)
-st.map(filtered_data)
+p_by_age = st.slider('Возраст', value=(30, 60));
+filtered_data = core.find_men_by_age(p_by_age[0], p_by_age[1]);
+st.dataframe(filtered_data);
 
 
-st.subheader('Класс, имя и возраст спасенных, имя которых начинается с:')
+st.subheader('Имена пасажиров, стоймость билета которых выше:');
 
-p_name = st.text_input('Имя:')
-filtered_data = core.find_passengers_by_name_start(p_name)
-st.map(filtered_data)
+ticket_cost = st.number_input('Стоимость билета:');
+filtered_data = core.get_names_by_fare(ticket_cost);
+st.dataframe(filtered_data);
+
+
+st.subheader('Класс, имя и возраст спасенных, имя которых начинается с:');
+
+p_name = st.text_input('Имя:');
+filtered_data = core.find_passengers_by_name_start(p_name);
+st.dataframe(filtered_data);
 
 
 
