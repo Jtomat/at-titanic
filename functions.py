0d975d07152d49022046f54cf4e56bdda9bffabb
Jtomat <stragnik13@gmail.com>
2023-05-17 18:31:53 +0500

    refactor: remove unused space 3

--- functions.py
+++ functions.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 class AppFunctions(object):
     dataFrame: pd.DataFrame
-    
+
     def __init__(self, source: str = None):
         if source is not None:
             self.dataFrame = pd.read_csv(source)
         else:
             self.dataFrame = pd.DataFrame()
-    
+
     def find_men_by_age(self, age_min: int, age_max: int) -> pd.DataFrame:
         if age_max > 60 or age_min < 30:
             return Exception('Age out of range [30; 60]')
@@ -18,11 +18,11 @@ class AppFunctions(object):
                  (self.dataFrame['Age'] >= age_min) &
                  (self.dataFrame['Age'] <= age_max)]
         return data_slice[['Name', 'Age', 'Pclass']]
-    
+
     def get_names_by_fare(self, fare: int) -> pd.DataFrame:
         data_slice: pd.DataFrame = self.dataFrame.loc[self.dataFrame["Fare"] > fare]
         return data_slice[["Name"]]
-    
+
     def find_passengers_by_name_start(self, name_start: str) -> pd.DataFrame:
         data_slice: pd.DataFrame = self.dataFrame \
             .loc[self.dataFrame['Name'].str.startswith(name_start)]
