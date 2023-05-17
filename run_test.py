be12ac5b690ac9433843732fd4f00d66737174fa
Augen-Auf <an_lesiv@mail.ru>
2023-05-17 18:38:47 +0500

    add test for get names by farw

--- run_test.py
+++ run_test.py
@@ -12,6 +12,12 @@ test_age_data_frame = pd.DataFrame({
 })
 
 
+test_fare_data_frame = pd.DataFrame({
+    "Name": ["Braund, Mr. Owen Harris"],
+    "Fare": [7.25]
+})
+
+
 def test_data_source_read():
     """
     Метод для проверки загрузки набора данных
@@ -27,3 +33,15 @@ def test_find_men_by_age(test_data: pd.DataFrame = test_age_data_frame, age_min:
     instance = AppFunctions()
     instance.dataFrame = test_data
     assert instance.find_men_by_age(age_min, age_max)["Name"][0] == "Braund, Mr. Owen Harris"
+
+
+def test_get_names_by_fare(test_data: pd.DataFrame = test_fare_data_frame, fare: float = 7):
+    """
+    Метод для проверки имен пассажиров, стоймость билета которых выше указанной.
+    """
+    instance = AppFunctions()
+    instance.dataFrame = test_data
+    assert instance.get_names_by_fare(fare)["Name"][0] == "Braund, Mr. Owen Harris"
+
+
+test_get_names_by_fare()
