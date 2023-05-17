f4a51b8851e42c6a3925b528f700542a0fec9e10
Igor Krupkin <krupkin.ig@mail.ru>
2023-05-17 18:38:33 +0500

    set test for passengers by name function

--- run_test.py
+++ run_test.py
@@ -11,6 +11,12 @@ test_age_data_frame = pd.DataFrame({
     "Age": [32],
 })
 
+test_name_start_data_frame = pd.DataFrame({
+    "Age": [20],
+    "Name": ["Cumings, Mrs. John Bradley (Florence Briggs Thayer)"],
+    "Pclass": [1],
+})
+
 
 def test_data_source_read():
     """
@@ -27,3 +33,16 @@ def test_find_men_by_age(test_data: pd.DataFrame = test_age_data_frame, age_min:
     instance = AppFunctions()
     instance.dataFrame = test_data
     assert instance.find_men_by_age(age_min, age_max)["Name"][0] == "Braund, Mr. Owen Harris"
+
+def test_find_passengers_by_name_start(test_data: pd.DataFrame = test_name_start_data_frame , name_start: str = "Cumings"):
+    """
+    Метод для проверки поиска мужчин в возрастном диапозоне.
+    """
+    instance = AppFunctions()
+    instance.dataFrame = test_data
+    res = instance.find_passengers_by_name_start(name_start)
+    assert res["Name"][0] == "Cumings, Mrs. John Bradley (Florence Briggs Thayer)" \
+    and res["Age"][0] == 20 \
+    and res["Pclass"][0] == 1
+
+test_find_passengers_by_name_start()
